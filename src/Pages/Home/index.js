feaa09f10658a4a7b2ced660c050d6b449d72241
RobertHJones <roberthuwjones@outlook.com>
2023-01-16 15:39:06 +0000

    fix

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -78,7 +78,7 @@ import API_URL from "../../config";
 const Home = () => {
   const { user, isAuthenticated } = useAuth0();
   const [spaces] = useFetch(`${API_URL}/spaces/`);
-  const arrays = spaces.slice(0, 7);
+  const arrays = spaces.payload.slice(0, 7);
   const [users] = useFetch(`${API_URL}/users/`);
 
   const navigate = useNavigate();
